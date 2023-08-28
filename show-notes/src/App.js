0e4112aae0fcb477a07eab0cbf02d891ca8a658f
Spencer Jankowski <spencerjan56@gmail.com>
2023-08-28 10:19:28 -0700

    Got the tiles working again. Displaying correctly. Clickable and routing correctly. Next steps include setting the right routes, right images, and displaying what I want in each route

--- show-notes/src/App.js
+++ show-notes/src/App.js
@@ -1,70 +1,27 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomeTile from './components/HomeTile';
-import './App.css';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 
+import HomeTile from './components/HomeTile'
+import tileDataArray from './TileData'
 
-function App() {
-  return (
-    <div className="home-tiles-container">
-      <Router>
-        <Routes>
-          <Route path="/" element={tilesObject.map((tile, index) => (
-            <HomeTile key={index} imageUrl={tile.imageUrl} routeTo={tile.routeTo} />
-          ))} />
-        </Routes>
-      </Router>
-    </div>
-  );
+
+export default function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div className="home-tiles-container">
+            {tileDataArray.map((tileData, index) => (
+              <HomeTile
+                key={index}
+                imageUrl={tileData.imageUrl}
+                routeTo={tileData.routeTo}
+              />
+            ))}
+          </div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-
-
-
-
-
-
-const tilesObject = [
-  {imageUrl: 'https://steamuserimages-a.akamaihd.net/ugc/1466437268076310022/3453C2187E128B756E9611A1F7890CC88507CE1E/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true',
-  routeTo: '/route1',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/08/Episode-Card-138-David-Linden.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-135-Tony-Hawk.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route1',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-134-Maya-Shankar-1.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-132-Rob-Malenka.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-131.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route1',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://steamuserimages-a.akamaihd.net/ugc/1466437268076310022/3453C2187E128B756E9611A1F7890CC88507CE1E/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true',
-  routeTo: '/route1',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://steamuserimages-a.akamaihd.net/ugc/1466437268076310022/3453C2187E128B756E9611A1F7890CC88507CE1E/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true',
-  routeTo: '/route1',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://steamuserimages-a.akamaihd.net/ugc/1466437268076310022/3453C2187E128B756E9611A1F7890CC88507CE1E/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true',
-  routeTo: '/route1',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',},
-  {imageUrl: 'https://i0.wp.com/hubermanlab.com/wp-content/uploads/2023/07/Episode-Card-133.jpg?resize=1080%2C608&ssl=1',
-  routeTo: '/route2',}
-]
-
-
-
-export default App;
--- show-notes/src/components/HomeTile.js
+++ show-notes/src/components/HomeTile.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+//import tileDataArray from '../TileData'
 import '../App.css';
+
+
+
 
 const HomeTile = ({imageUrl, routeTo}) =>{
     return (
+        
         <Link to= {routeTo} className="home-tile">
-            <img src={imageUrl} alt="Clickable Image" />
+            <img src={imageUrl} alt=""/>
         </Link>
 
     );
 };
-
 
 
 export default HomeTile;
